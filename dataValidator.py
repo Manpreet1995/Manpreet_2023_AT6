066ae6db1afd6eb28e401957d070c74e86c8ee04
Manpreet1995 <manpreet.13-12@hotmail.com>
2023-12-14 07:53:01 +0100

    Third commit: changed variable names
    changed variable name in validation method

--- dataValidator.py
+++ dataValidator.py
@@ -1,7 +1,7 @@
 class main:
     def validate(self, list_of_inputs):
         numbers = []
-        for item in list_of_inputs:
-            if int(item) > 0 and item.isdigit():
-                numbers.append(int(item))
+        for the_input in list_of_inputs:
+            if int(the_input) > 0 and the_input.isdigit():
+                numbers.append(int(the_input))
         return numbers
