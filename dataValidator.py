a0bee233b8be72bee2b4ef547e9caeffbac005e1
Manpreet1995 <manpreet.13-12@hotmail.com>
2023-12-14 07:50:49 +0100

    Second commit: improved code
    added checking if input is a number to validation method

--- dataValidator.py
+++ dataValidator.py
@@ -2,6 +2,6 @@ class main:
     def validate(self, list_of_inputs):
         numbers = []
         for item in list_of_inputs:
-            if int(item) > 0:
+            if int(item) > 0 and item.isdigit():
                 numbers.append(int(item))
         return numbers
